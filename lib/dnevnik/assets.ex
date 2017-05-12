9b3459f55f5090673bf15c2f492674984888eb71
cyberdot <contact@cyberdot.co.uk>
2017-05-12 17:02:55 +0100

    Css and js files location handling

--- lib/dnevnik/assets.ex
+++ lib/dnevnik/assets.ex
@@ -6,29 +6,33 @@ defmodule Dnevnik.Assets do
   
   @spec css() :: String.t
   def css do
-    css_files()
-    |> Enum.map(&("<link rel=\"stylesheet\" href=\"#{&1}\" />"))
-    |> Enum.join("\n")
+    css_dir = "#{Config.public_directory}/assets/css"
+	case File.exists?(css_dir) do
+		true -> css_files(css_dir) |> Enum.map(&("<link rel=\"stylesheet\" href=\"#{&1}\" />")) |> Enum.join("\n")
+		false -> ""
+	end
   end
 
   @spec js() :: String.t
   def js do
-    js_files()
-    |> Enum.map(&("<script type=\"text/javascript\" src=\"#{&1}\"></script>"))
-    |> Enum.join("\n")
+    js_dir = "#{Config.public_directory}/assets/js"
+	case File.exists?(js_dir) do
+		true -> js_files(js_dir) |> Enum.map(&("<script type=\"text/javascript\" src=\"#{&1}\"></script>")) |> Enum.join("\n")
+		false -> ""
+	end
   end
 
   @spec css_files() :: list 
-  defp css_files do
-    File.ls!("#{Config.public_directory}/assets/css")
+  defp css_files(css_dir) do
+    File.ls!(css_dir)
     |> Enum.sort
     |> Enum.map(&("assets/css/#{&1}"))
     |> Enum.filter(&(!File.dir? "#{Config.public_directory}/#{&1}"))
   end
 
   @spec js_files() :: list
-  defp js_files do
-    File.ls!("#{Config.public_directory}/assets/js")
+  defp js_files(js_dir) do
+    File.ls!(js_dir)
     |> Enum.sort
     |> Enum.map(&("assets/js/#{&1}"))
     |> Enum.filter(&(!File.dir? "#{Config.public_directory}/#{&1}"))
